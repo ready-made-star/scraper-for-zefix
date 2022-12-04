4bf46447d1203e39d2301f170cdffab5a3e56d97
ready-made-star <maskymlap@gmail.com>
2022-12-04 06:30:58 -0800

    internal scraper

--- zefix.py
+++ zefix.py
@@ -9,69 +9,47 @@ import requests
 import json
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support import expected_conditions as EC
 number = [4533, 108978, 224594, 393159]
+driver = webdriver.Chrome(executable_path='./chromedriver.exe')
+driver.maximize_window()
+base_url="https://www.zefix.admin.ch/fr/search/entity/list/firm/{}"
+# /html/body/zfx-root/main/zfx-entity/zfx-firm/div/div[2]/a[1]
+external_url = []
+n = 0
+for x in number:
+    main_url=base_url.format(x)
+    driver.get(main_url)
+    # external_url.append(driver.find_element(By.XPATH, '/html/body/zfx-/main/zfx-entity/zfx-firm/div/div[2]/a[1]').get_attribute('href'))
 
-base_url = "https://www.zefix.admin.ch/ZefixREST/api/v1/firm/"
+    companyAction_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'companyActions')))
+    if companyAction_element:
 
-ua = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-headers = {'User-Agent': 'PostmanRuntime/7.26.8', 'Accept': '*/*',
-           'Accept-Encoding': 'gzip, deflate, br', 'Connection': 'keep-alive'}
-shab = []
-tagesregister = []
-mutationen = []
-detail = []
-for x in range(len(number)):
-    get_url = (base_url + str(number[x]) + "/shabPub.json")
-    response = requests.get(get_url, headers=headers)
-    products = json.loads(response.text)
+        a_element = companyAction_element.find_element(By.TAG_NAME, "a").get_attribute("href")
+        external_url.append(a_element)
 
-    for y in products:
+    item_length = driver.find_elements(By.CLASS_NAME, "shabPub")
+    for i in item_length:
+        # item = driver.find_elements(By.XPTH, ("//div[@class= 'shabPub')")[2])
+        n = n + 1
+        items = i.find_elements(By.TAG_NAME, "td")
 
-        shab.append("Nr." + str(y['shabId']) + " vom [" + y['shabDate'] + "]")
-        tagesregister.append("Nr." + str(y['registryOfficeJournalId']) + " vom [" + y['registryOfficeJournalDate'] + "]")
-        mutationen
+        row = []
+        for item in items:
 
-# def scrapProducts():
+            row.append(item.text)
+        with open('event3.csv', 'a', encoding='utf-8', newline='') as f_object:
 
-#     for x in range(product_page_num):
-#         product_get_url = (
-#             product_origin_url + "&page={}&page_size={}&latlng=33.83475%2C-117.91173").format(x+1, product_page_per_take)
-#         # print(product_get_url)
-#         response = requests.get(product_get_url, headers=headers)
-#         # print(response.text)
-#         products = json.loads(response.text)['data']['products']
-#         for y in products:
-#             productList_slug.append(y['slug'])
-#             productList_name.append(y['name'])
-#             productList_url.append(y['avatar_image_url'])
-#             productList_review_count.append(y['reviews_count'])
-#             productList_thc.append(y['variant']['aggregate_metrics']['thc'])
-#             productList_cbd.append(y['variant']['aggregate_metrics']['cbd'])
-#             try:
-#                 productList_desc.append(y['brand']['description'])
-#             except:
-#                 productList_desc.append('')
-
-#             productList_rating.append(y['rating'])
-#             productList_price.append(y['variant']['price']['price'])
-
-# scrapProducts()
-
-# df = pd.DataFrame({
-#     'name': productList_name,
-#     'slug': productList_slug,
-#     'url': productList_url,
-#     'thc': productList_thc,
-#     'cbd': productList_cbd,
-#     'rating': productList_rating,
-#     'review_count': productList_review_count,
-#     'description': productList_desc,
-#     'price': productList_price,
-
-# })
-
-# df.to_excel('weed_product.xlsx', index=False, encoding='utf-8')
-# print('weedmap_product_done')
+            product_row = writer(f_object)
+            product_row.writerow(row)
+            f_object.close()
+        
+for y in external_url:
+    driver.get(y)
+    table_element = driver.find_element(By.CLASS_NAME, "personen")
+    tr_element = table_element.find_element(By.TAG_NAME, "tr").text
+    print(tr_element)
+    # row_element = table_element.
+    
